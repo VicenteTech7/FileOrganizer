bbfd2fa3ac2d96eac74fb3597ad4e593c4d1b980
vicentetech7 <cybersec@gmail.com>
2026-07-23 20:56:43 +0200

    Añadida validación de la carpeta introducida por el usuario

--- organizador.py
+++ organizador.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
 def seleccionar_carpeta():
-    ruta: str = input("¿Qué carpeta quieres organizar?")
-    print(f"La carpeta elegida es: {ruta}")
+    ruta = input("¿Qué carpeta quieres organizar?")
+    carpeta = Path(ruta)
+
+    if carpeta.exists() and carpeta.is_dir():
+        print(f"\n✓ Carpeta encontrada : {carpeta}")
+    else:
+        print("\n✗ La carpeta no existe.")
+
 def main():
     print("=" * 40)
     print("     FILE ORGANIZER v0.2")
     print("=" * 40)
     seleccionar_carpeta()
+
 if __name__ == "__main__":
     main()
