617e8efcef7845f24b4f10d30253872fe73d2938
vicentetech7 <cybersec@gmail.com>
2026-07-23 20:14:46 +0200

    Reorganizada la estructura inicial del programa

--- organizador.py
+++ organizador.py
@@ -1,4 +1,10 @@
-ruta = input("¿Qué carpeta quieres organizar?")
-
-print("La carpeta elegida es:")
-print(ruta)
+def seleccionar_carpeta():
+    ruta: str = input("¿Qué carpeta quieres organizar?")
+    print(f"La carpeta elegida es: {ruta}")
+def main():
+    print("=" * 40)
+    print("     FILE ORGANIZER v0.2")
+    print("=" * 40)
+    seleccionar_carpeta()
+if __name__ == "__main__":
+    main()
